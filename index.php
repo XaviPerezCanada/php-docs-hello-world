d5f3032b1126dd629b0e147e53d9d8c5e4dc1667
XaviPerezCanada <157647268+XaviPerezCanada@users.noreply.github.com>
2026-02-18 20:22:12 +0100

    Probando rutaas

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+// Prueba de ruta absoluta
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
 
-echo "¡Hola Mundo!";
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+    echo "¡Autoload cargado con éxito! <br>";
+} else {
+    die("ERROR: No se encuentra el archivo en: " . $autoloadPath);
+}
 
+// Intentar usar una clase de Azure
+if (class_exists('MicrosoftAzure\Storage\Blob\BlobRestProxy')) {
+    echo "La clase BlobRestProxy está disponible.";
+} else {
+    echo "Error: La librería de Azure no se ha cargado correctamente.";
+}
 ?>
